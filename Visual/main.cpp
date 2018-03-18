b9a9db4d5a1ad890503c1055a9dec0629fd57037
pautovartem <pautov.artem1998@gmail.com>
2018-03-18 11:02:07 +0200

    remove border GameWindow
    Co-Authored-By: AlexanderPK <alexanderpk@users.noreply.github.com>

--- Visual/main.cpp
+++ Visual/main.cpp
@@ -1,10 +1,12 @@
-#include "gamewindow.h"
+#include "GameWindow.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     GameWindow w;
+    w.setWindowFlags(Qt::Window | Qt::FramelessWindowHint);
     w.show();
 
     return a.exec();

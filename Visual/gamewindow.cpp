1b093af5eccad9a4b869047e02890eb71ad41ce0
pautovartem <pautov.artem1998@gmail.com>
2018-04-09 10:07:01 +0300

    Fix scale

--- Visual/gamewindow.cpp
+++ Visual/gamewindow.cpp
@@ -13,7 +13,7 @@ GameWindow::GameWindow(QWidget *parent) :
     gameGround = new GameGround();
 
     ui->graphicsView->setScene(gameGround);
-    ui->graphicsView->scale(0.2, 0.2);
+    ui->graphicsView->scale(1, 1);
     ui->graphicsView->setRenderHint(QPainter::Antialiasing);
     ui->graphicsView->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     ui->graphicsView->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
